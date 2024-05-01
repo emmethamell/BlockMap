10696dc1422f118bf64188e790464e940d5a0231
mrper <115744059+tomperature@users.noreply.github.com>
2024-05-01 13:25:27 -0400

    implemented getBuildingAndRoomCodes() function

--- data/helpers.mjs
+++ data/helpers.mjs
@@ -1,4 +1,5 @@
 import XLSX from "xlsx";
+import { buildingCodes } from "./building-codes.mjs";
 
 function convertTime(excelDateTime) {
     let excelTime = excelDateTime % 1; 
@@ -21,6 +22,28 @@ function getData() {
     const worksheet = workbook.Sheets[sheetName];
 
     return XLSX.utils.sheet_to_json(worksheet);
+}
+
+/** 
+ * Takes a location string and returns the correctly formatted building code and room code.
+ * @param {string} locationString location string from excel sheet
+ * @returns {[string, string]} [building_code, room_code]
+ */
+function getBuildingAndRoomCodes(locationString) {
+    // remove all hyphens (if they exist)
+    locationString = locationString.replace("-", ""); 
+    /* 
+     * Get building code. This works because the building code list is in ascending order,
+     * i.e. "BCABZSN" comes before "BCA". If location string starts with "BCABZSN", then its 
+     * building code will be correctly identified as "BCABZSN", and not "BCA". If the list
+     * were in descending order, it would be identified first as "BCA", which is *not* correct. 
+     */
+    let building = buildingCodes.find(code => locationString.startsWith(code)); 
+    // if location isn't a part of a valid builing, throw error
+    if (building === undefined) { throw new Error("Invalid building code."); } 
+    // get room code (all characters after building code, leading zeros removed)
+    let room = locationString.substring(building.length).replace(/^0+/, "");
+    return [building, room];
 }
 
 
