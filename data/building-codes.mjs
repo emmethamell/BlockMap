6c74a4a7e4a686587bd33f39e1c97195429b6672
tomperature <115744059+tomperature@users.noreply.github.com>
2024-05-01 12:04:19 -0400

    Create building-codes.mjs
    Has standardized valid building codes

---
+++ data/building-codes.mjs
@@ -0,0 +1,77 @@
+export const buildingCodes = [
+    "AEBC", // Ag. Engin. Central
+    "AEBCXB", // Ag. Engin. Bldg Annex B
+    "AEBN", // Ag. Engineering Bldg
+    "ARND", // Arnold Hall
+    "BART", // Bartlett Hall
+    "BCA", // Bromery Center for Arts
+    "BCABZSN", // Bromery Center Bezanson Hall
+    "BFLD", // Butterfield House
+    "BOWD", // Bowditch Hall
+    "BOYD", // Boyden Gym
+    "CC", // Campus Center
+    "CHEN", // Chenoweth Lab addn.
+    "CHNW", // Chenoweth Lab
+    "CHNWPLNT", // Chenoweth Pilot Plant
+    "CMPS", // Computer Science Building
+    "CNTE", // Conte Polymer Res Ctr
+    "CURTTHEAT", // Curtain Theater
+    "DB", // Design Building
+    "DKSN", // Dickinson Hall
+    "DRA", // Draper Hall
+    "DWGT", // Dwight House
+    "ELAB", // Engineering Lab
+    "ELABII", // Engineering Lab II
+    "ELM", // Elm Hall/House
+    "EMER", // Emerson Hall
+    "FERN", // Fernald Hall
+    "FLIN", // Flint Laboratory
+    "FREN", // French Hall
+    "FURC", // Furcolo Hall
+    "GOES", // Goessmann Hal
+    "GORDNHALL", // Gordon Hall
+    "GORM", // Gorman House
+    "GP", // George Parks Bldg 
+    "GSMN", // Goessmann Hall addn.
+    "GUN", // Gunness Hall
+    "HAS", // Hasbrouck Laboratory
+    "HASA", // Hasbrouck Laboratory addn.
+    "HERT", // Herter Hall
+    "HICKPOOL", // Curry Hicks Phys. Ed Bldg Pool
+    "HOLD", // Holdsworth Hall
+    "ILC", // Integrative Learning Center
+    "IS",  // Integrated Science Labs
+    "ISB", // Integrated Sciences Building
+    "LGRC", // Lederle Graduate Research Center
+    "LGRT", // Lederle Graduate Research Tower
+    "LIB", // WEB DuBois Library
+    "LSL", // Life Sciences Lab
+    "LYON", // Mary Lyon House
+    "MACH", // Machmer Hall
+    "MAH", // Mahar Auditorium
+    "MARC", // Marcus Hall  
+    "MELV", // Melville Hall
+    "MONT", // Montague House
+    "MOR", // Morrill
+    "MOR1", // Morrill I
+    "MOR2", // Morrill II
+    "MOR3", // Morrill III
+    "MOR4", // MOrrill IV
+    "MRST", // Marston Hall
+    "NAH", // New Africa House
+    "PAIG", // Paige Laboratory
+    "RANDSTAGE", // Rand Stage 
+    "RANDUPPR", // Rand Upper Lobby 
+    "REG", // Research & Educ Greenhouse
+    "SAB", // Studio Arts Building
+    "SC", // South College
+    "SKIN", // Skinner Hall
+    "SOM", // School of Management
+    "STK", // Stockbridge Hall
+    "THCH", // Thatcher House
+    "THOM", // Thompson Hall
+    "TOBN", // Tobin Hall
+    "TOTM", // Totman Gym
+    "WHEEL", // Wheeler Hall
+    "WORC", // Worcester Dining Hall
+];
